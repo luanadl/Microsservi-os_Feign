326c0558c01f0e94c4b4f946f2e814e0eddb6bcb
luanadl <luanadl@ciandt.com>
2022-08-08 21:36:21 -0300

    Adicionando as Annotations do JPA e Integrando o Servico ao Banco de Dados

--- cambio-service/src/main/java/br/com/erudio/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/erudio/cambioservice/controller/CambioController.java
@@ -1,6 +1,7 @@
 package br.com.erudio.cambioservice.controller;
 
 import br.com.erudio.cambioservice.model.Cambio;
+import br.com.erudio.cambioservice.repository.CambioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping("cambio-service")
@@ -18,6 +20,9 @@ public class CambioController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    CambioRepository repository;
+
     // http://localhost:8000/cambio-service/5/USD/BRL
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(
@@ -25,8 +30,16 @@ public class CambioController {
             @PathVariable("from") String from,
             @PathVariable("to") String to) {
 
-        var port = environment.getProperty("local.server.port");
+        var cambio = repository.findByFromAndTo(from, to);
+        if (cambio == null) throw new RuntimeException("Currency Unsupported");
 
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
+        var port = environment.getProperty("local.server.port");
+        cambio.setEnvironment(port);
+
+        BigDecimal conversionFactor = cambio.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+
+        return  cambio;
     }
 }
---
+++ cambio-service/src/main/java/br/com/erudio/cambioservice/repository/CambioRepository.java
@@ -0,0 +1,9 @@
+package br.com.erudio.cambioservice.repository;
+
+import br.com.erudio.cambioservice.model.Cambio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CambioRepository extends JpaRepository<Cambio, Long> {
+
+    Cambio findByFromAndTo(String from, String to);
+}
