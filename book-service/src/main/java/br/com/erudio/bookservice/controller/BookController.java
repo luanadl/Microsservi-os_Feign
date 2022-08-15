e4ce0f90a63e11297d4d8ce238125c27a7dcdc0c
luanadl <luanadl@ciandt.com>
2022-08-15 14:12:51 -0300

    Microsservico Book

---
+++ book-service/src/main/java/br/com/erudio/bookservice/controller/BookController.java
@@ -0,0 +1,38 @@
+package br.com.erudio.bookservice.controller;
+
+import br.com.erudio.bookservice.model.Book;
+import br.com.erudio.bookservice.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private BookRepository repository;
+
+
+    @GetMapping(value = "/{id}/{currency}")
+    public Book findBook(
+            @PathVariable("id") Long id,
+            @PathVariable("currency") String currency
+    ) {
+
+        var book = repository.getReferenceById(id); //getById
+        if (book == null) throw new RuntimeException("Book not Found");
+
+
+        var port = environment.getProperty("local.server.port");
+        book.setEnvironment(port) ;
+
+        return book;
+    }
+}
