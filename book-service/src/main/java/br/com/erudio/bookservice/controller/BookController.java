76d6de96cd972d2b47696edd52483930b834a922
luanadl <luanadl@ciandt.com>
2022-08-15 16:11:53 -0300

    Consumindo Endpoint Cambio Service no Book Service

--- book-service/src/main/java/br/com/erudio/bookservice/controller/BookController.java
+++ book-service/src/main/java/br/com/erudio/bookservice/controller/BookController.java
@@ -2,12 +2,16 @@ package br.com.erudio.bookservice.controller;
 
 import br.com.erudio.bookservice.model.Book;
 import br.com.erudio.bookservice.repository.BookRepository;
+import br.com.erudio.bookservice.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -19,7 +23,6 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
-
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(
             @PathVariable("id") Long id,
@@ -29,9 +32,23 @@ public class BookController {
         var book = repository.getReferenceById(id); //getById
         if (book == null) throw new RuntimeException("Book not Found");
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response =  new RestTemplate()
+                .getForEntity("http://localhost:8001/cambio-service/" + "{amount}/{from}/{to}",
+                        Cambio.class,
+                        params);
+
+        var cambio = response.getBody();
 
         var port = environment.getProperty("local.server.port");
         book.setEnvironment(port) ;
+
+        assert cambio != null;
+        book.setPrice(cambio.getConvertedValue());
 
         return book;
     }
